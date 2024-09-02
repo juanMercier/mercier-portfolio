fbef8b6b1b14e8b9ba73b6e994308da6fdf90762
juanMercier <joaomercier@gmail.com>
2024-09-02 21:54:08 +0100

    initial commit

---
+++ components/Nav.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+    {
+        name: "home",
+        path: "/",
+    },
+    {
+        name: "services",
+        path: "/services",
+    },
+    {
+        name: "work",
+        path: "/work",
+    },
+    {
+        name: "resume",
+        path: "/resume",
+    },
+    {
+        name: "contact",
+        path: "/contact",
+    },
+]
+
+
+
+export default function Nav() {
+    const pathname = usePathname();
+
+    return (
+        <nav className="flex gap-8">
+            {links.map((link, index) => {
+                return <Link
+                    href={link.path}
+                    className={`${link.path === pathname && "text-accent border-b-2 border-accent"} 
+                    capitalize font-medium hover:text-accent transition-all`}
+                    key={index}>
+                    {link.name}
+                </Link>
+            })}
+        </nav>
+    );
+}
