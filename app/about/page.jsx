fa51f0dcccfac36fb3bd383e6118014e7c0d4fbf
juanMercier <joaomercier@gmail.com>
2024-09-10 02:41:01 +0100

    final touches and content

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -171,7 +171,7 @@ export default function About() {
                 opacity: 1,
                 transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }
             }}
-            className='min-h-[80vh] flex flex-col justify-center py-12 xl:px-0'
+            className='flex flex-col justify-center py-12 xl:px-0'
         >
             <div className="container mx-auto">
                 <Tabs defaultValue='experience' className='flex flex-col xl:flex-row gap-[60px]'
@@ -252,7 +252,7 @@ export default function About() {
                             <div className='flex flex-col gap-[30px]'>
                                 <h3 className='text-4xl font-bold'>{about.title}</h3>
                                 <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>{about.description}</p>
-                                <ul className='grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0'>
+                                <ul className='grid grid-cols-1 xl:grid-cols-2 gap-y-6 gap-x-10 max-w-[620px] mx-auto xl:mx-0'>
                                     {about.info.map((item, index) => {
                                         return <li key={index} className='flex items-center justify-center xl:justify-start gap-4'>
                                             <span className='text-white/60'>{item.fieldName}</span>
